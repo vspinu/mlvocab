894114ce973f1589c76e3b27c72dc9ca0a1f251c
Vitalie Spinu <spinuvit@gmail.com>
2018-04-10 23:37:03 +0200

    Add extra dim tests

--- tests/testthat/test_term_matrices.R
+++ tests/testthat/test_term_matrices.R
@@ -137,23 +137,31 @@ test_that("dtm works", {
   dtm <- dtm(corpus, v, output = "row")
   expect_s4_class(dtm, "dgRMatrix")
   expect_equal(as.matrix(dtm), mat)
-
-  ## names(corpus) <- NULL
-  ## rownames(mat) <- NULL
-
-  ## dtm <- dtm(corpus, v, output = "triplet")
-  ## expect_s4_class(dtm, "dgTMatrix")
-  ## expect_equal(as.matrix(dtm), mat)
-
-  ## dtm <- dtm(corpus, v, output = "column")
-  ## expect_s4_class(dtm, "dgCMatrix")
-  ## expect_equal(as.matrix(dtm), mat)
-
-  ## dtm <- dtm(corpus, v, output = "row")
-  ## expect_s4_class(dtm, "dgRMatrix")
-  ## expect_equal(as.matrix(dtm), mat)
   
 })
+
+test_that("dtm works with NULL names", {
+
+  names(corpus) <- NULL
+  rownames(mat) <- NULL
+
+  dtm <- dtm(corpus, v, output = "triplet")
+  expect_s4_class(dtm, "dgTMatrix")
+  expect_equal(dim(dtm), dim(mat))
+  expect_equal(as.matrix(dtm), mat)
+
+  dtm <- dtm(corpus, v, output = "column")
+  expect_s4_class(dtm, "dgCMatrix")
+  expect_equal(dim(dtm), dim(mat))
+  expect_equal(as.matrix(dtm), mat)
+
+  dtm <- dtm(corpus, v, output = "row")
+  expect_s4_class(dtm, "dgRMatrix")
+  expect_equal(dim(dtm), dim(mat))
+  expect_equal(as.matrix(dtm), mat)
+  
+})
+
 
 test_that("tdm works", {
 
@@ -169,21 +177,29 @@ test_that("tdm works", {
   tdm <- tdm(corpus, v, output = "row")
   expect_s4_class(tdm, "dgRMatrix")
   expect_equal(as.matrix(tdm), mat)
+  
+})
 
-  ## names(corpus) <- NULL
-  ## colnames(mat) <- NULL
 
-  ## tdm <- tdm(corpus, v, output = "triplet")
-  ## expect_s4_class(tdm, "dgTMatrix")
-  ## expect_equal(as.matrix(tdm), mat)
+test_that("tdm works with NULL names", {
 
-  ## tdm <- tdm(corpus, v, output = "column")
-  ## expect_s4_class(tdm, "dgCMatrix")
-  ## expect_equal(as.matrix(tdm), mat)
+  names(corpus) <- NULL
+  colnames(mat) <- NULL
 
-  ## tdm <- tdm(corpus, v, output = "row")
-  ## expect_s4_class(tdm, "dgRMatrix")
-  ## expect_equal(as.matrix(tdm), mat)
+  tdm <- tdm(corpus, v, output = "triplet")
+  expect_s4_class(tdm, "dgTMatrix")
+  expect_equal(dim(tdm), dim(mat))
+  expect_equal(as.matrix(tdm), mat)
+
+  tdm <- tdm(corpus, v, output = "column")
+  expect_s4_class(tdm, "dgCMatrix")
+  expect_equal(dim(tdm), dim(mat))
+  expect_equal(as.matrix(tdm), mat)
+
+  tdm <- tdm(corpus, v, output = "row")
+  expect_s4_class(tdm, "dgRMatrix")
+  expect_equal(dim(tdm), dim(mat))
+  expect_equal(as.matrix(tdm), mat)
   
 })
 
